48712cb626ce2275c6d69d682efc826a45841bf2
saahilkhan06 <saahilslaptop@gmail.com>
2025-07-12 08:55:23 +0000

    datatypes

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+let empid = 101;
+let empname = "John Doe";
+var empdesignation = "Software Engineer";
+
+empid = 102;
+empname = "sam";
+empdesignation = "Software Engineer";
+// console.table([empid, empname, empdesignation]);
+console.log(typeof null);
+console.log(typeof undefined);
+
+ 
