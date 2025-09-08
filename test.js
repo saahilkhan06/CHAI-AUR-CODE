103b1525743a85f7c601d25bbe9ee7bb1d185c0f
saahilkhan06 <saahilslaptop@gmail.com>
2025-09-08 13:52:42 +0000

    filtermap

--- test.js
+++ test.js
@@ -482,13 +482,75 @@
 //     }
 
 // const coding = ["java","js",'c','c++','python']
-// coding.forEach( (val) =>  {
+// const values=coding.forEach( (val) =>  {
 //     console.log(val);
+//     return val
     
 // })
+// console.log(values);
+
 // function printme(val) {
 //     console.log(val);
 // }
 // coding.forEach(printme)
 // coding.forEach((val,index,array) =>{
 // })
+// const mynum = [1,2,3,4,5,6,7,8]
+// const newnum=mynum.filter((num) =>{
+//     return num             >4
+    
+// // })
+// const newnum =[]
+// mynum.forEach((num) => {
+//     if (num>4) {
+//         newnum.push(num)
+//     }
+// })
+// console.log(newnum);
+
+// const books = [
+//   { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//   { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//   { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//   { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//   { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//   { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//   { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+// ];
+
+// const userbook = books.filter((bks)=>(bks.genre==="History"))
+// const userbooks = books.filter((pbs)=>{
+//      return pbs.publish>1995
+//     })
+// console.log(userbooks);
+// console.log(userbook); 
+//  const newnum = mynum.forEach((num) => {
+//     return num+10
+//  })
+//  console.log(newnum);
+// const mynum = [1,2,3,4,5,6,7,8]
+// const newnum=mynum
+// .map((num)=>num+10)
+// .map((num)=>num+5)
+// .filter((num)=>num>20)
+// console.log(newnum);
+// const array = [1, 2, 3, 4];
+
+// // 0 + 1 + 2 + 3 + 4
+// const initialValue = 0;
+// const sumWithInitial = array.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   initialValue
+// );
+
+// console.log(sumWithInitial);
+const mynum = [1,2,3,4,5]
+const initialnum= 0
+const newnum=mynum.reduce((acc,cvalue) => {return`current balance is${acc} and added wiht${cvalue}`})
+console.log(newnum);
+
+ 
+
+
+
+
